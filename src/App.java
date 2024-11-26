b66a9ab1e7beface9811c67149322552d881a223
Manana-19 <Mananjerath9582@gmail.com>
2024-11-26 22:54:56 +0530

    v0.1 -> It Works...

--- src/App.java
+++ src/App.java
@@ -9,44 +9,23 @@ public class App {
     static String x = String.valueOf(0);
     public static int counter = 0;
 
-    static void initialize(Scanner inp) {
+    static double initialize(Scanner inp) {
         
         if (!history.isEmpty()) {
-            history.clear();
-            System.out.print("\nPlease enter your initial value ->");
-            double initial = inp.nextDouble();
-
-
-            String[] arr = {"Initial Digits",String.valueOf(initial)};
-
-            history.add(arr);
+            history.clear(); // Clearing the List
         }
+
+        System.out.print("\nPlease enter your initial value ->");
+
+        double initial = inp.nextDouble();
+
+        String[] arr = {"Initial Digits",String.valueOf(initial)};
+    
+        history.add(arr);
+
+        return initial;
 
     };
-
-    static String method(int x) {
-        String toReturn = "x";
-
-        switch(x) {
-            case(1):
-                toReturn="Addition";
-                break;
-            case(2):
-                toReturn="Subtraction";
-                break;
-            case(3):
-                toReturn="Multiplication";
-                break;
-            case(4):
-                toReturn="Division";
-                break;
-            case(5):
-                toReturn="Square Root";
-                break;
-        }
-
-        return toReturn;
-    }
 
     static void HistoryCheck() {
         for(String[] x : history) {
@@ -59,8 +38,8 @@ public class App {
         // Creating Strings for the format
         String initialString = "Welcome to this CLI Calculator !!! \nNote: This calculator can take up to 999 instructions";
         String calculate = "1 -> Addition\n2 -> Subtraction\n3 -> Multiplication\n4 -> Division\n5 -> Square Root\n0 -> History\n\n\n9 -> Reset";
-        String valueInsert = "Please enter the next value";
-        String expInsert = "Please Select the Expression";
+        String valueInsert = "Please enter the next value\n -> ";
+        String expInsert = "Please Select the Expression\n -> ";
 
         // Initializing
 
@@ -70,22 +49,19 @@ public class App {
         Scanner inp = new Scanner(System.in);
 
         // Try and catch block because the code below are now risky ones where errors could occur
+
         try {
 
-            System.out.print("\nPlease enter your initial value ->");
-            double initial = inp.nextDouble();
-
-            String[] arr = {"Initial Digits",String.valueOf(initial)};
-
-            history.add(arr);
-            
+            double value = initialize(inp); // Initializing
 
             while (true && counter < 999) {
+
+                System.out.println("Current Value --> "+value);
                 
                 System.out.print("\n"+calculate+"\n"+expInsert);
                 int val = inp.nextInt();
 
-                if (val != 6 || val != 7 || val != 8) { // I forgot about how to check if a variable is in a array or not
+                if (val == 6 || val == 7 || val == 8) { // I forgot about how to check if a variable is in a array or not
                     
                     System.out.println("\n Not a valid option... please try again...");
                     continue;
@@ -93,17 +69,60 @@ public class App {
                 } else if (val == 9) {
 
                     System.out.println("Reinitiallizing....");
-                    initialize(inp);
-                    System.gc();
+
+                    value = initialize(inp); // Restarting the calculator
+                    System.gc(); // Clearing some garbage just in case
+
                     continue;
 
-                } else if (val == 6) {
-                    HistoryCheck();
+                } else if (val == 0) {
+
+                    HistoryCheck(); // Using that HistoryCheck functionm
                     continue;
+
+                } else if (val == 5) { // Special Case for Square Root Function
+
+                    double res = cal.sqroot(value);
+                    String[] toAdd = {String.valueOf(res),"Square Root"};
+                    history.add(toAdd);
+                    value = res;
+                    continue;
+
                 } else {
 
-                    // Code for the calculations
-                
+                    System.out.print("\n"+valueInsert);
+                    double toUse = inp.nextDouble();
+
+                    double newValue = 0; // Just for the initializing sake, It will be changed in the following switch case
+
+                    switch((int) val) {
+                        case(1):
+                            newValue = cal.sum(toUse,value);
+                            // Using Primitive value coz I am bored (I know I could've used StringBuffer but it's boring....)
+                            String[] toAdd = {String.valueOf(newValue),"Addition with "+String.valueOf(toUse)};
+                            history.add(toAdd);
+                            break;
+                        case(2):
+                            newValue = cal.sub(value, toUse);
+                            String[] toSub = {String.valueOf(newValue),"Subtraction with "+String.valueOf(toUse)};
+                            history.add(toSub);
+                            break;
+                        case(3):
+                            newValue = cal.mul(value, toUse);
+                            String[] toMultiply = {String.valueOf(newValue),"Multiplication with "+String.valueOf(toUse)};
+                            history.add(toMultiply);
+                            break;
+                        case(4):
+                            
+                            if (value == 0) {System.out.println("Error: Divided by 0");inp.close();return;}
+
+                            newValue = cal.div(value, toUse);
+                            String[] toDivide = {String.valueOf(newValue),"Division with "+String.valueOf(toUse)};
+                            history.add(toDivide);
+                            break;
+                    }
+                    value = newValue;
+                    continue;
                 }
 
             }
--- src/c_lib/cal.java
+++ src/c_lib/cal.java
@@ -1,19 +1,22 @@
 package c_lib;
 
 public class cal {
-    public double sum(double a, double b) {
+    public static double sum(double a, double b) {
         return a + b;
     };
-    public double sub(double a, double b) { 
+    public static double sub(double a, double b) { 
         return a - b;
     };
-    public double mul(double a, double b ) {
+    public static double mul(double a, double b ) {
         if (a == 0 || b == 0) return 0;
         return a * b;
     };
     @SuppressWarnings("null")
-    public double div(double a, double b) {
+    public static double div(double a, double b) {
         if (b == 0) return (Double) null;
         else return a/b;
+    }
+    public static double sqroot(double x) {
+        return Math.sqrt(x);
     }
 }
